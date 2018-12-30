0aa65283e7d011d2542e8368e168a93cd44bf6a1
Horst Jahns <horstjahns@gmail.com>
2018-12-30 14:31:09 +0100

    v0.0.5 - adding form for element type of area

---
+++ src/components/Editor/Overlay/ElementDetail/ElementTypes/Area.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slider from '../FormElements/Slider/Slider';
+import Input from '../FormElements/Input/Input';
+
+const Area = () => {
+  return(
+    <div>
+      <Input />
+      <Slider min={10} max={60} defaultValue={20} propertyName="width" />
+      <Slider min={0} max={360} defaultValue={0} step={15} propertyName="rotationZ" />
+    </div>
+  );
+}
+
+export default Area
