0e84e8fb092d424ee5b8de3061ea39f4f7e51fad
Horst Jahns <horstjahns@gmail.com>
2019-01-06 05:20:10 +0100

    v0.0.10 flow integration
    - adding flow lib
    - exclude dependencies
    - fix react-redux bugs
    - use flow to define elements shape
    - update npm packages
    - add flow dependencies to package.json
    - add flow to sample file

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,27 +1,32 @@
+// @flow
 import React, { Component } from 'react';
 import Presentation from '../Presentation/Presentation';
 import './Editor.css';
 import EditorController from '../../controller/EditorController';
 import PositionController from '../../controller/PositionController';
 import { connect } from 'react-redux';
-import { addElement, editElement } from '../../actions/actions';
+import { editElement } from '../../actions/actions';
 import Overlay from './Overlay/Overlay';
 
-class Editor extends Component {
-  constructor(props) {
+type Props = {
+  editElement: Function,
+  elements: Array<Object>
+};
+
+class Editor extends Component<Props> {
+  constructor(props: Props) {
       super(props);
-      this.handleOnKeyDown = this.handleOnKeyDown.bind(this);
+      (this:any).handleOnKeyDown = this.handleOnKeyDown.bind(this);
   }
 
-  handleOnKeyDown(event) {
+  handleOnKeyDown(event: Object) {
     let activeElement = EditorController.getActiveElement(this.props.elements);
     let movedElement = PositionController.handleOnKeyDown(activeElement, event);
     let changed = PositionController.positionChanged(activeElement, movedElement);
     if (changed) {
       this.props.editElement(movedElement);
-    } 
+    }
   }
-
 
   render() {
     return (
@@ -35,12 +40,10 @@ class Editor extends Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   elements: state.elements
 });
 
-const mapDispatchToProps = {
-  addElement, editElement
-};
+const mapDispatchToProps = {editElement};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor);
